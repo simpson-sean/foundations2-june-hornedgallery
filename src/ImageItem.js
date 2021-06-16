c14d3dcee035f65940ad83e96013b9a91a17e9ca
Sean <simpson.sean@outlook.com>
2021-06-16 08:14:32 -0700

    Images now rendering properly through ImageItem

--- src/ImageItem.js
+++ src/ImageItem.js
@@ -1,14 +1,21 @@
 import React, { Component } from 'react'
 
 export default class ImageItem extends Component {
+    
     render() {
         //the cool zone
         console.log(this.props.pokedex)
 
         return (
             <div>
-                This is image Item.
+              {this.props.pokedex.map(image =>     
+                <div className="animal-list">
+                    <img src={image.url} alt={image.keyword} width='200px' height='200px' />
+                    {/* <p>Title: {image.title}</p>
+                    <p>Description: {image.description}</p>
+                    <p>Horn Count:{image.horns}</p> */}
+                </div> 
+              )}
             </div>
-        )
-    }
+        )}
 }
